8cffbc997770e26a9073e4d372d6f27b3eb8c73d
Thiago Ramos <thiago.s.ramos@outlook.com>
2020-07-06 21:05:07 -0300

    Iniciando estrutura do projeto

---
+++ InternshipProject/ViewModel/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InternshipProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
